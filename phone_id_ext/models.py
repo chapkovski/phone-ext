50c243ba2a26ce392b756ff43fa8d74ed79d901d
chapkovski <chapkovski@gmail.com>
2018-01-28 15:10:18 -0500

    updating tables

--- phone_id_ext/models.py
+++ phone_id_ext/models.py
@@ -1,9 +1,4 @@
-from otree.api import (
-    models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
-    Currency as c, currency_range
-)
-
-from django.db import models as djmodels
+from django.db import models
 
 author = 'Your name here'
 
@@ -14,15 +9,26 @@ Your app description
 from otree.models import Participant, Session
 
 
-class AddedSession(djmodels.Model):
-    session = djmodels.OneToOneField(to=Session)
+# Models for phone identification
+class LinkedSession(models.Model):
+    session = models.OneToOneField(to=Session, on_delete=models.CASCADE)
+
+class PhoneRecord(models.Model):
+    phone_id = models.IntegerField()
+    linked_session=models.ForeignKey(to=LinkedSession)
+    participant=models.ForeignKey(to=Participant)
+
+class AddedSession(models.Model):
+    session = models.OneToOneField(to=Session)
+
     def __str__(self):
         return 'Added session {}'.format(self.session.code)
 
 
-class ParticipantEmailLookup(djmodels.Model):
-    email = djmodels.EmailField(blank=True, null=True)
-    participant = djmodels.OneToOneField(to=Participant)
+class ParticipantEmailLookup(models.Model):
+    email = models.EmailField(blank=True, null=True)
+    participant = models.OneToOneField(to=Participant)
+
     def __str__(self):
         participant_url = self.participant._url_i_should_be_on()
         return 'PEL: {}, {}'.format(self.email, participant_url)
